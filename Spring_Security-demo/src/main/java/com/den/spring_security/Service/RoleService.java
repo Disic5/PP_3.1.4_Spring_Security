56daeb3b8f825b2c2740bb1e74e065ccf6337615
Denis <svd.denis@mail.ru>
2022-04-12 12:22:05 +0300

    3.1.3

---
+++ Spring_Security-demo/src/main/java/com/den/spring_security/Service/RoleService.java
@@ -0,0 +1,12 @@
+package com.den.spring_security.Service;
+
+import com.den.spring_security.Model.Role;
+
+import java.util.List;
+import java.util.Set;
+
+public interface RoleService {
+    public Set<Role> findRoles(List<Long> roles);
+
+    public List<Role> getAllRoles();
+}
